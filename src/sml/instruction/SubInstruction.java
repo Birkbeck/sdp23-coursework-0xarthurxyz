6f155cf58b2a25bdf82d43790c684bbb5d6ee4d7
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-15 22:43:32 +0000

    feat(SubInstruction): adds constructor

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -8,5 +8,18 @@ public class SubInstruction extends Instruction {
 	private final RegisterName source;
 
 	public static final String OP_CODE = "sub";
+
+	/**
+	 * Constructor: 
+	 * 
+	 * @param label
+	 * @param result
+	 * @param source
+	 */
+	public SubInstruction(String label, RegisterName result, RegisterName source) {
+		super(label, OP_CODE);
+		this.result = result;
+		this.source = source;
+	}
     
 }
