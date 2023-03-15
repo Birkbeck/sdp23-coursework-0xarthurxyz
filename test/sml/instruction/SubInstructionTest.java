ea65dbb4af19f5ced74f663bcf07e20dbd426e9a
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-15 22:39:38 +0000

    test(SubInstructionTest): adds skeleton and basic test

---
+++ test/sml/instruction/SubInstructionTest.java
@@ -0,0 +1,43 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+/**
+ * Provides the unit tests with access to the enumeration {@code Registers} defined
+ * in the {@code Register} class.
+ */
+import static sml.Registers.Register.*;
+
+class SubInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        // ...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new SubInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(11, machine.getRegisters().get(EAX));
+    }
+}
