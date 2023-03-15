03dc61fe107445d41e032e037159b11b9f3370fb
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-15 23:31:05 +0000

    docs(SubInstruction): adds javadoc

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -14,9 +14,9 @@ public class SubInstruction extends Instruction {
 	 * 
 	 * @param label		optional name given to this instruction; label name can be used to jump to
 	 * 					this instruction from other instructions.
-	 * @param result	name of the register in which the result of the addition will be stored;
-	 * 					also name of the register whose value will be used in the addition.
-	 * @param source	name of the register whose value will be used in the addition.
+	 * @param result	name of the register in which the result of the subtraction will be stored;
+	 * 					also name of the register whose value will be used in the subtraction.
+	 * @param source	name of the register whose value will be used in the subtraction.
 	 */
 	public SubInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
