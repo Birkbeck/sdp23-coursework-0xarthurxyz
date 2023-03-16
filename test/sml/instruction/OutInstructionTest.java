54bffc5eba33d743b8774d7a9cac85716f88a000
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 12:31:24 +0000

    test(OutInstruction): adds opcode test

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -12,5 +12,26 @@ import sml.Registers;
 import static sml.Registers.Register.*;
 
 class OutInstructionTest {
-  
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    // ...
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void givenInstruction_whenInstantiating_thenOpcodeCorrect() {
+    registers.set(EAX, 5);
+    Instruction instruction = new OutInstruction(null, EAX);
+    Assertions.assertEquals(instruction.getOpcode(), "out");
+  }
 }
