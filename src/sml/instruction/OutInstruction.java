1a15b1264539881f17da8f2edb80512b16a9cc2f
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 12:20:00 +0000

    feat(OutInstruction): adds imports

--- src/sml/instruction/OutInstruction.java
+++ src/sml/instruction/OutInstruction.java
@@ -1,5 +1,13 @@
 package sml.instruction;
 
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+// TODO: write a JavaDoc for the class
+/**
+ * @author
+ */
 public class OutInstruction extends Instruction {
   
 }
