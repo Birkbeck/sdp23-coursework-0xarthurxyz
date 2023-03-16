b1b3e973507d7609d10f4cbff5fbcca5acb2fc6d
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 10:43:38 +0000

    feat(MovInstruction): adds class fields

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -22,7 +22,7 @@ public class AddInstruction extends Instruction {
 	 * Constructor: Instantiates an addition instruction given two register names.
 	 * 
 	 * @param label		optional name given to this instruction; label name can be used to jump to 
-	 * 					this instruction from other instructions.
+	 * 					instruction from other instructions.
 	 * @param result	name of the register in which the result of the addition will be stored;
 	 * 					also name of the register whose value will be used in the addition.
 	 * @param source	name of the register whose value will be used in the addition.
--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -1,5 +1,18 @@
 package sml.instruction;
 
-public class MovInstruction {
+import sml.RegisterName;
+
+// TODO: write a JavaDoc for the class
+/**
+ * @author
+ */
+public class MovInstruction extends Instruction {
+    // My TODO: Add javadoc
+    private final RegisterName result;
+    // My TODO: Add javadoc
+	private final int VALUE;
+    // My TODO: Add javadoc
+	public static final String OP_CODE = "mov";
+
     
 }
