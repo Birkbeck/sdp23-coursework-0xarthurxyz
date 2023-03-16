24dfa3c8fb2d7a45bc09dcfb9a20515de3209893
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:33:28 +0000

    feat(MulInstruction): implements execute method

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -31,4 +31,23 @@ public class MulInstruction extends Instruction {
 		this.result = result;
 		this.source = source;
 	}
+
+  /** 
+	 * Performs a state transition on a given machine.
+	 * 
+	 * <p> Multiplies the value of a register by the value of another register 
+   * and stores that result in a register.
+	 * 
+	 * @param m	Machine object with a given set of registers 
+	 * @return 	the new program counter (for jump instructions)
+	 * 			or NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
+	 * 			the instruction with the next address is to be executed
+	 */
+	@Override
+	public int execute(Machine m) {
+		int value1 = m.getRegisters().get(result);
+		int value2 = m.getRegisters().get(source);
+		m.getRegisters().set(result, value1 * value2);
+		return NORMAL_PROGRAM_COUNTER_UPDATE;
+	}
 }
