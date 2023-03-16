2bddf162c098973f34fe4bf6ebe29e4fbe8e58af
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:29:44 +0000

    test(MulInstructionTest): adds execute, toString, equals and hashCode tests

--- test/sml/instruction/MulInstructionTest.java
+++ test/sml/instruction/MulInstructionTest.java
@@ -1,5 +1,72 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
 public class MulInstructionTest {
-  
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    // ...
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void givenSimpleValues_whenMultiplying_thenResultIsCorrect() {
+    registers.set(EAX, 5);
+    registers.set(EBX, 6);
+    Instruction instruction = new MulInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(30, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void givenPositiveAndNegativeValues_whenMultiplying_thenResultIsCorrect() {
+    registers.set(EAX, -5);
+    registers.set(EBX, 6);
+    Instruction instruction = new MulInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(-30, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void givenEquivalentInstructions_whenComparing_thenTrue() {
+    registers.set(EAX, 6);
+    registers.set(EBX, 5);
+    // Instantiates first instruction
+    Instruction firstInstruction = new MulInstruction(null, EAX, EBX);
+    // Instantiates second instruction
+    Instruction SecondInstruction = new MulInstruction(null, EAX, EBX);
+    // Compares equivalent instructions
+    Assertions.assertTrue(firstInstruction.equals(SecondInstruction));
+  }
+
+  @Test
+  void givenEquivalentInstructions_whenHashCodes_thenEquivalent() {
+    registers.set(EAX, 6);
+    registers.set(EBX, 5);
+    // Instantiates first instruction
+    Instruction firstInstruction = new MulInstruction(null, EAX, EBX);
+    // Instantiates second instruction
+    Instruction SecondInstruction = new MulInstruction(null, EAX, EBX);
+    // Compares equivalent instructions
+    Assertions.assertEquals(firstInstruction.hashCode(), SecondInstruction.hashCode());
+  }
 }
