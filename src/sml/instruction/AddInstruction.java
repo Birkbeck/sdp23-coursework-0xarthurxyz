e8270e11b578d3df3fd494da5112958bc4f713d1
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 20:56:44 +0000

    feat(AddInstruction): implements equal method - passes test as expected

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -1,5 +1,7 @@
 package sml.instruction;
 
+import java.util.Objects;
+
 import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
@@ -58,10 +60,15 @@ public class AddInstruction extends Instruction {
 	}
 	
 	// My TODO: Add javadoc
-	// My TODO: Implement equals method
 	@Override
 	public boolean equals(Object o) {
-		// temporary implementation to stop compilation errors
+		// Uses `instanceof` pattern matching.
+		if (o instanceof AddInstruction other) {
+			// The binding variable `other` can be used in this scope
+			// because the `instanceof` predicate evaluates to true in this block.
+			return Objects.equals(this.result, other.result)
+					&& Objects.equals(this.source, other.source);
+		}
 		return false;
 	}
 	
