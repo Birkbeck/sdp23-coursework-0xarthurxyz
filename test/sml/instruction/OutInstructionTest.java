a7a556e9d3801795e589907632c1e6665ce76636
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 12:30:24 +0000

    test(OutInstruction): adds imports:

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -1,5 +1,16 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
 class OutInstructionTest {
   
 }
