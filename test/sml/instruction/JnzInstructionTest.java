79a5a2d50de066a2d64c68376c57b3857fd56dd4
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 22:24:14 +0000

    test(JnzInstructionTest): adds test when register != 0

--- test/sml/instruction/JnzInstructionTest.java
+++ test/sml/instruction/JnzInstructionTest.java
@@ -32,11 +32,22 @@ class JnzInstructionTest {
 
   @Test
   void givenRegisterEqualsZero_whenExecutingJnz_thenContinueSequentialExecution() {
-    // Set source register to 0
+    // Set source register to zero
     registers.set(EAX, 0);
     Instruction instruction = new JnzInstruction(null, EAX, "anyLabel");
     // Assert programCounter is equal to NORMAL_PROGRAM_COUNTER_UPDATE
-    Assertions.assertEquals( instruction.execute(machine), NORMAL_PROGRAM_COUNTER_UPDATE );
+    Assertions.assertEquals(NORMAL_PROGRAM_COUNTER_UPDATE, instruction.execute(machine));
+  }
+
+  @Test
+  void givenRegisterNonZero_whenExecutingJnz_thenContinueAtLabel() {
+    // Set source register to some non-zero value
+    registers.set(EAX, 1);
+    // Set a label point to the instruction stored at index 10
+    machine.getLabels().addLabel("someLabel", 10); 
+    Instruction instruction = new JnzInstruction(null, EAX, "someLabel");
+    // Assert programCounter is equal to NORMAL_PROGRAM_COUNTER_UPDATE
+    Assertions.assertEquals(10, instruction.execute(machine));
   }
 
   @Test
