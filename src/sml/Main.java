010e0d604a0d09d27dcf7b3a7dbfa39baae02cd9
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 18:50:47 +0000

    feat(Main): comments out official instructions and test basic Translator (passes as expected)

--- src/sml/Main.java
+++ src/sml/Main.java
@@ -2,6 +2,11 @@ package sml;
 
 import java.io.IOException;
 
+//
+// My TODO: Remove unofficial imports below
+//
+import static sml.Registers.Register.*;
+
 public class Main {
 	/**
 	 * Initialises the system and executes the program.
@@ -9,24 +14,49 @@ public class Main {
 	 * @param args name of the file containing the program text.
 	 */
 	public static void main(String... args) {
-		if (args.length != 1) {
-			System.err.println("Incorrect number of arguments - Machine <file> - required");
-			System.exit(-1);
-		}
+		//
+		// My TODO: Uncomment offifical instructions below:
+		//
+
+		// if (args.length != 1) {
+		// 	System.err.println("Incorrect number of arguments - Machine <file> - required");
+		// 	System.exit(-1);
+		// }
 
 		try {
-			Translator t = new Translator(args[0]);
+			//
+			// My TODO: Remove my personal instructions below
+			//
+			Translator t = new Translator("test3.sml");
 			Machine m = new Machine(new Registers());
 			t.readAndTranslate(m.getLabels(), m.getProgram());
 
+
 			System.out.println("Here is the program; it has " + m.getProgram().size() + " instructions.");
-			System.out.println(m);
 
 			System.out.println("Beginning program execution.");
 			m.execute();
 			System.out.println("Ending program execution.");
+			System.out.println("Values of register is: " + m.getRegisters().get(EAX));
 
-			System.out.println("Values of registers at program termination:" + m.getRegisters() + ".");
+
+			//
+			// My TODO: Uncomment offifical instructions below:
+			//
+
+			// Translator t = new Translator(args[0]);
+			// Machine m = new Machine(new Registers());
+			// t.readAndTranslate(m.getLabels(), m.getProgram());
+
+			// My TODO: Uncomment official instructions
+			// System.out.println("Here is the program; it has " + m.getProgram().size() + " instructions.");
+			// System.out.println(m);
+
+			// System.out.println("Beginning program execution.");
+			// m.execute();
+			// System.out.println("Ending program execution.");
+
+			// System.out.println("Values of registers at program termination:" + m.getRegisters() + ".");
 		}
 		catch (IOException e) {
 			System.out.println("Error reading the program from " + args[0]);
