288bc457f4520a49f91f419e14df6e9a8b911d0f
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:31:24 +0000

    feat(MulInstruction): adds class fields and imports

---
+++ src/sml/instruction/MulInstruction.java
@@ -0,0 +1,19 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+// TODO: write a JavaDoc for the class
+
+/**
+ * @author
+ */
+public class MulInstruction extends Instruction {
+  // My TODO: Add javadoc
+	private final RegisterName result;
+	// My TODO: Add javadoc
+	private final RegisterName source;
+	// My TODO: Add javadoc
+	public static final String OP_CODE = "add";
+}
