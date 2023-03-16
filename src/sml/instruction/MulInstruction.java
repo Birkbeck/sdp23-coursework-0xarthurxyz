c51d50675e4c3af5fd8f5bd01249c8f35362f156
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:32:27 +0000

    feat(MulInstruction): implements constructor

--- src/sml/instruction/MulInstruction.java
+++ src/sml/instruction/MulInstruction.java
@@ -16,4 +16,19 @@ public class MulInstruction extends Instruction {
 	private final RegisterName source;
 	// My TODO: Add javadoc
 	public static final String OP_CODE = "add";
+
+  /**
+	 * Constructor: Instantiates a multiplication instruction given two register names.
+	 * 
+	 * @param label		optional name given to this instruction; label name can be used to jump to 
+	 * 					      this instruction from other instructions.
+	 * @param result	name of the register in which the result of the addition will be stored;
+	 * 					      also name of the register whose value will be used in the addition.
+	 * @param source	name of the register whose value will be used in the addition.
+	 */
+	public MulInstruction(String label, RegisterName result, RegisterName source) {
+		super(label, OP_CODE);
+		this.result = result;
+		this.source = source;
+	}
 }
