97106250c26cfc1b567b05baeb596f81557a2b74
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 19:27:50 +0000

    test(JnzInstructionTest): adds incomplete test, missing equals implementation

--- test/sml/instruction/JnzInstructionTest.java
+++ test/sml/instruction/JnzInstructionTest.java
@@ -1,5 +1,46 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
 class JnzInstructionTest {
-  
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    // ...
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void givenRegisterSetToZero_whenExecutingJnz_thenContinueSequentialExecution() {
+    // Set source register to 0
+    registers.set(EAX, 0);
+    // Execute JnzInstruction
+    Instruction instruction = new JnzInstruction(null, EAX, "testLabel");
+    // Assert programCounter is equal to NORMAL_PROGRAM_COUNTER_UPDATE
+    Assertions.assertEquals(instruction.getOpcode(), "add");
+    // Construct next instruction
+    // Instruction nextInstruction = new MovInstruction(null, EBX, 2)
+    // Assert next instruction is equal
+    // Assertions.assertEquals(program.get(programCounter), nextInstruction);
+  }
+
+
 }
