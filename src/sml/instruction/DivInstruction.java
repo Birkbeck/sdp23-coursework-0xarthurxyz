fe77c79e2b2c9bda2c6465fdea50d96a54543453
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:57:45 +0000

    feat(DivInstruction): adds constructor

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -9,12 +9,27 @@ import sml.RegisterName;
 /**
  * @author
  */
-public class DivInstruction {
+public class DivInstruction extends Instruction {
   // My TODO: Add javadoc
-	private final RegisterName result;
-	// My TODO: Add javadoc
-	private final RegisterName source;
-	// My TODO: Add javadoc
-	public static final String OP_CODE = "div";
+  private final RegisterName result;
+  // My TODO: Add javadoc
+  private final RegisterName source;
+  // My TODO: Add javadoc
+  public static final String OP_CODE = "div";
+
+  /**
+	 * Constructor: Instantiates a division instruction given two register names.
+	 * 
+	 * @param label		optional name given to this instruction; label name can be used to jump to 
+	 * 					      this instruction from other instructions.
+	 * @param result	name of the register in which the result of the addition will be stored;
+	 * 					      also name of the register whose value will be used in the addition.
+	 * @param source	name of the register whose value will be used in the addition.
+	 */
+	public DivInstruction(String label, RegisterName result, RegisterName source) {
+		super(label, OP_CODE);
+		this.result = result;
+		this.source = source;
+	}
 
 }
