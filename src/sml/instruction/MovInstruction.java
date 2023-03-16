942abd0907540c7a2e56771e152aa8baa27db43e
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 10:55:45 +0000

    feat(MovInstruction): implements constructor

--- src/sml/instruction/MovInstruction.java
+++ src/sml/instruction/MovInstruction.java
@@ -10,9 +10,21 @@ public class MovInstruction extends Instruction {
     // My TODO: Add javadoc
     private final RegisterName result;
     // My TODO: Add javadoc
-	private final int VALUE;
+	private final int value;
     // My TODO: Add javadoc
 	public static final String OP_CODE = "mov";
 
-    
+    /**
+	 * Constructor: Instantiates a move instruction given a register name and an integer value.
+	 * 
+	 * @param label		optional name given to this instruction; label name can be used to jump to 
+	 * 					this instruction from other instructions.
+	 * @param result	name of the register in which the integer value will be stored
+	 * @param value 	integer value that  will be stored in the register
+	 */
+	public MovInstruction(String label, RegisterName result, int value) {
+		super(label, OP_CODE);
+		this.result = result;
+		this.value = value;
+	}
 }
