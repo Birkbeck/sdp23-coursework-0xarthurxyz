f999d72f7ff0be02fca3ed374d04b1fe6071e4c5
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 19:08:35 +0000

    feat(JnzInstruction): implements constructor

--- src/sml/instruction/JnzInstruction.java
+++ src/sml/instruction/JnzInstruction.java
@@ -11,7 +11,23 @@ public class JnzInstruction extends Instruction {
 	// My TODO: Add javadoc
 	private final RegisterName source;
   // My TODO: Add javadoc
-	private final String label;
+	private final String destinationLabel;
 	// My TODO: Add javadoc
 	public static final String OP_CODE = "jnz";
+
+  /**
+	 * Constructor: Instantiates a "jump if not zero" instruction given a register name
+   * and an label name to jump to.
+	 * 
+	 * @param label		optional name given to this instruction; label name can be used to jump to 
+	 * 					this instruction from other instructions.
+	 * @param source	name of the register whose value will be compared to 0.
+	 * @param label	  name of the instruction to which the program should jump if the value at
+   *          register {@code source} is equal to 0.
+	 */
+	public JnzInstruction(String label, RegisterName source, String destinationLabel) {
+		super(label, OP_CODE);
+		this.source = source;
+		this.destinationLabel = destinationLabel;
+	}
 }
