d2121811a85364f9f763df6acfc98ceb44961222
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 19:02:07 +0000

    feat(JnzInstruction): adds class fields

--- src/sml/instruction/JnzInstruction.java
+++ src/sml/instruction/JnzInstruction.java
@@ -1,9 +1,17 @@
 package sml.instruction;
 
+import sml.Instruction;
+import sml.RegisterName;
+
 // TODO: write a JavaDoc for the class
 /**
  * @author
  */
 public class JnzInstruction extends Instruction {
-  
+	// My TODO: Add javadoc
+	private final RegisterName source;
+  // My TODO: Add javadoc
+	private final String label;
+	// My TODO: Add javadoc
+	public static final String OP_CODE = "jnz";
 }
