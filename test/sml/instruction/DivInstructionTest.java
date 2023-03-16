4b9aa1fd193f69c9de3ea6fd25e8c364e1a0562e
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 11:56:27 +0000

    test(DivInstruction): adds tests for opcode, execute, toString, equals and hashCode

--- test/sml/instruction/DivInstructionTest.java
+++ test/sml/instruction/DivInstructionTest.java
@@ -12,5 +12,69 @@ import sml.Registers;
 import static sml.Registers.Register.*;
 
 class DivInstructionTest {
-  
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    // ...
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void givenInstruction_whenInstantiating_thenOpcodeCorrect() {
+    registers.set(EAX, 100);
+    registers.set(EBX, 2);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    Assertions.assertEquals(instruction.getOpcode(), "div");
+  }
+
+  @Test
+  void givenSimpleValues_whenAdding_thenResultIsCorrect() {
+    registers.set(EAX, 100);
+    registers.set(EBX, 2);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(50, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void givenPositiveAndNegativeValues_whenAdding_thenResultIsCorrect() {
+    registers.set(EAX, 100);
+    registers.set(EBX, -2);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(-50, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void givenEquivalentInstructions_whenComparing_thenTrue() {
+    registers.set(EAX, 100);
+    registers.set(EBX, 2);
+    // Instantiates first instruction
+    Instruction firstInstruction = new DivInstruction(null, EAX, EBX);
+    // Instantiates second instruction
+    Instruction SecondInstruction = new DivInstruction(null, EAX, EBX);
+    // Compares equivalent instructions
+    Assertions.assertTrue(firstInstruction.equals(SecondInstruction));
+  }
+
+  @Test
+  void givenEquivalentInstructions_whenHashCodes_thenEquivalent() {
+    registers.set(EAX, 100);
+    registers.set(EBX, 2);
+    // Instantiates first instruction
+    Instruction firstInstruction = new DivInstruction(null, EAX, EBX);
+    // Instantiates second instruction
+    Instruction SecondInstruction = new DivInstruction(null, EAX, EBX);
+    // Compares equivalent instructions
+    Assertions.assertEquals(firstInstruction.hashCode(), SecondInstruction.hashCode());
+  }
 }
