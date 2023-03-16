d1dfd505d7f2783c17c257641efff6344bd848b6
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-16 10:47:31 +0000

    test(MovInstructionTest): adds basic mov test

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -1,5 +1,37 @@
 package sml.instruction;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
 public class MovInstructionTest {
-    
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        // ...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void givenPositiveInteger_whenMoving_thenRegisterIsSetCorrectly() {
+        Instruction instruction = new MovInstruction(null, EAX, 1);
+        instruction.execute(machine);
+        Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+    }
 }
