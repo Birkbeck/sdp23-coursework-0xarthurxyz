f8e7a6b5091db3156c4842b7cc0cf4fb7470d7d1
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-17 19:42:46 +0000

    docs(Machine): adds javadoc

--- src/sml/Machine.java
+++ src/sml/Machine.java
@@ -7,84 +7,109 @@ import java.util.stream.Collectors;
 
 import static sml.Instruction.NORMAL_PROGRAM_COUNTER_UPDATE;
 
-
 enum supportedOpcodes implements OpcodeClassName {
-  add {
-    @Override
-    public String className() {
-      return "AddInstruction";
-    }
-  }, 
+	add {
+		@Override
+		public String className() {
+			return "sml.instruction.AddInstruction";
+		}
+	},
 
-  sub {
-    @Override
-    public String className() {
-      return "SubInstruction";
-    }
-  }, 
-  
-  mul {
-    @Override
-    public String className() {
-      return "MulInstruction";
-    }
-  },
-  
-  div {
-    @Override
-    public String className() {
-      return "DivInstruction";
-    }
-  },
+	sub {
+		@Override
+		public String className() {
+			return "sml.instruction.SubInstruction";
+		}
+	},
 
-  mov {
-    @Override
-    public String className() {
-      return "MovInstruction";
-    }
-  },
+	mul {
+		@Override
+		public String className() {
+			return "sml.instruction.MulInstruction";
+		}
+	},
 
-  out {
-    @Override
-    public String className() {
-      return "OutInstruction";
-    }
-  },
-  
-  jnz {
-    @Override
-    public String className() {
-      return "JnzInstruction";
-    }
-  }
+	div {
+		@Override
+		public String className() {
+			return "sml.instruction.DivInstruction";
+		}
+	},
+
+	mov {
+		@Override
+		public String className() {
+			return "sml.instruction.MovInstruction";
+		}
+	},
+
+	out {
+		@Override
+		public String className() {
+			return "sml.instruction.OutInstruction";
+		}
+	},
+
+	jnz {
+		@Override
+		public String className() {
+			return "sml.instruction.JnzInstruction";
+		}
+	}
 }
 
 /**
  * Represents the machine, the context in which programs run.
  * 
- * <p> An instance contains 8 registers and methods to access and change them.
+ * <p>
+ * An instance contains 8 registers and methods to access and change them.
+ * 
+ * @author Arthur Gousset
+ */
+
+/**
+ * This class defines all relevant methods and holds all relevant data
+ * to execute from a plaintext file into internal form.
+ * 
+ * <p>
+ * This class is used to parse instructions from a plaintext file and initiate a
+ * machine ready to execute the program.
+ *
  * @author Arthur Gousset
  */
 public final class Machine {
-	// My TODO: add javadoc
+	/**
+	 * This fields represents the set of labels that can be used to jump across instructions
+	 * in the program.
+	 */
 	private final Labels labels = new Labels();
-	// My TODO: add javadoc
+	/**
+	 * This field represents the instructions that can be executed by the machine.
+	 */
 	private final List<Instruction> program = new ArrayList<>();
-	// My TODO: add javadoc
+	/**
+	 * This field represents the logical memory of the machine.
+	 */
 	private final Registers registers;
-
-	// The program counter; it contains the index (in program)
-	// of the next instruction to be executed.
+	/**
+	 * The program counter which contains the index in the program
+	 * of the next instruction to be executed.
+	 */
 	private int programCounter = 0;
 
-	// Constructor
+	/**
+	 * Constructor: Instantiates a machine object with a specific state.
+	 * 
+	 * @param registers the set of register and their values with which to
+	 *                  instantiate the machine
+	 */
 	public Machine(Registers registers) {
 		this.registers = registers;
 	}
 
-	// My TODO: add better javadoc
 	/**
-	 * Execute the program in program, beginning at instruction 0.
+	 * Executes the instructions stored in {@code program}, beginning at instruction
+	 * 0.
 	 * Precondition: the program and its labels have been stored properly.
 	 */
 	public void execute() {
@@ -94,14 +119,14 @@ public final class Machine {
 			Instruction ins = program.get(programCounter);
 			int programCounterUpdate = ins.execute(this);
 			programCounter = (programCounterUpdate == NORMAL_PROGRAM_COUNTER_UPDATE)
-				? programCounter + 1
-				: programCounterUpdate;
+					? programCounter + 1
+					: programCounterUpdate;
 		}
 	}
 
 	public static String getOpcodeClassName(supportedOpcodes op) {
-  	return op.className();
-  }
+		return op.className();
+	}
 
 	// My TODO: add javadoc
 	public Labels getLabels() {
@@ -113,7 +138,11 @@ public final class Machine {
 		return this.program;
 	}
 
-	// My TODO: add javadoc
+	/**
+	 * Gets the registers of this machine.
+	 * 
+	 * @return
+	 */
 	public Registers getRegisters() {
 		return this.registers;
 	}
@@ -130,9 +159,17 @@ public final class Machine {
 				.collect(Collectors.joining("\n"));
 	}
 
-	// My TODO: consider removing comment below
-	// TODO: use pattern matching for instanceof
-	// https://docs.oracle.com/en/java/javase/14/language/pattern-matching-instanceof-operator.html
+	/**
+	 * Compares the state and type of this object to that of another object.
+	 * 
+	 * <p>
+	 * The state of this object is defined by the fields of this object, which is
+	 * why they are used to calculate unique hash codes for this class.
+	 * 
+	 * @param o an object to compare this object to.
+	 * @return {@code true} if the state and type of the objects is equal,
+	 *         {@code false} otherwise.
+	 */
 	@Override
 	public boolean equals(Object o) {
 		// Uses `instanceof` pattern matching.
@@ -147,8 +184,15 @@ public final class Machine {
 		return false;
 	}
 
-	// My TODO: Test hashCode implementation of Machine.java
-	// My TODO: add javadoc
+	/**
+	 * Produces a unique hash code for every possible state of this object.
+	 * 
+	 * <p>
+	 * The state of this object is defined by the fields of this object, which is
+	 * why they are used to calculate unique hash codes for this class.
+	 * 
+	 * @return a unique hash code
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(labels, program, registers, programCounter);
