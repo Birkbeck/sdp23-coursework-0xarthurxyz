925dc55564b8a0e1bcd0d8898fe93baf08081e2c
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-17 19:50:40 +0000

    feat(Registers): implements instanceof in equals method

--- src/sml/Registers.java
+++ src/sml/Registers.java
@@ -3,36 +3,35 @@ package sml;
 import java.util.*;
 import java.util.stream.Collectors;
 
-// TODO: write a JavaDoc for the class
-
 /**
  * 
- * This class is:
- * <ul>
- * <li> {@code public}, which means it is accessible by any other class
- * <li> {@code final}, which means it cannot be inherited by other classes
- * </ul>
+ * This class represents the logical memory of the machine.
+ * 
+ * <p>
+ * It defines all relevant methods and holds all relevant data
+ * to help the machine perform state transitions.
+ * 
  * @author Arthur Gousset
  */
 public final class Registers {
     /**
-     * The registers of the machine, which represent the memory locations 
+     * The registers of the machine, which represent the memory locations
      * that can be written to and read from to perform the instructions
      * supported by the machine.
      * 
-     * <p> The registers are implemented as a mapping from an element of 
+     * <p>
+     * The registers are implemented as a mapping from an element of
      * the {@code Register} enumeration to an {@code Integer}.
      * 
-     * <p> This field is:
+     * <p>
+     * This field is:
      * <ul>
-     * <li> {@code private}, which means it is only accessible in this class
-     * <li> {@code final}, which means it cannot be overridden/modified
+     * <li>{@code private}, which means it is only accessible in this class
+     * <li>{@code final}, which means it cannot be overridden/modified
      * </ul>
      */
     private final Map<Register, Integer> registers = new HashMap<>();
-    
-    // My TODO: check I don't need to implement the `name()` method of
-    // the RegisterName interface implemented by Register.
+
     /**
      * An enumeration of the register names available to be written to and
      * read from in the machine.
@@ -42,19 +41,18 @@ public final class Registers {
     }
 
     /**
-     * Constructor: produces a mapping from every element of 
+     * Constructor: produces a mapping from every element of
      * the {@code Register} enumeration to the integer 0.
      * 
-     * @return  mapping from every {@code Register} element to 
-     *          the integer 0.
+     * @return mapping from every {@code Register} element to
+     *         the integer 0.
      */
     public Registers() {
-        clear(); // the class is final
+        clear();
     }
-    
-    // My TODO: Consider using Java streams API instead of for-loop
+
     /**
-     * Resets all registers by setting every 
+     * Resets all registers by setting every
      * mapping from an element of the {@code Register} enumeration to the integer 0
      */
     public void clear() {
@@ -66,10 +64,10 @@ public final class Registers {
      * Sets the given register to the value.
      *
      * @param register register name
-     * @param value new value
+     * @param value    new value
      */
     public void set(RegisterName register, int value) {
-        registers.put((Register)register, value);
+        registers.put((Register) register, value);
     }
 
     /**
@@ -79,30 +77,55 @@ public final class Registers {
      * @return value
      */
     public int get(RegisterName register) {
-        return registers.get((Register)register);
+        return registers.get((Register) register);
     }
 
-    // TODO: use pattern matching for instanceof
-    // https://docs.oracle.com/en/java/javase/14/language/pattern-matching-instanceof-operator.html
+    /**
+     * Compares the state and type of this object to that of another object.
+     * 
+     * <p>
+     * The state of this object is defined by the fields of this object, which is
+     * why they are used to calculate unique hash codes for this class.
+     * 
+     * @param o an object to compare this object to.
+     * @return {@code true} if the state and type of the objects is equal,
+     *         {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object o) {
-        if (o instanceof Registers) {
-            Registers other = (Registers) o;
+        // Uses `instanceof` pattern matching.
+        if (o instanceof Registers other) {
+            // The binding variable `other` can be used in this scope
+            // because the `instanceof` predicate evaluates to true in this block.
             return registers.equals(other.registers);
         }
         return false;
     }
 
+    /**
+     * Produces a unique hash code for every possible state of this object.
+     * 
+     * <p>
+     * The state of this object is defined by the fields of this object, which is
+     * why they are used to calculate unique hash codes for this class.
+     * 
+     * @return a unique hash code
+     */
     @Override
     public int hashCode() {
         return registers.hashCode();
     }
 
+    /**
+     * Returns a formatted String representation of this object.
+     * 
+     * @return Nicely formatted String representation of the object.
+     */
     @Override
     public String toString() {
         return registers.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(e -> e.getKey() + " = " + e.getValue())
-                .collect(Collectors.joining(", ", "[", "]")) ;
+                .collect(Collectors.joining(", ", "[", "]"));
     }
 }
