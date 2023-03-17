6d276ffc152462521db391f1a98f0b9f2fdb099c
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-17 18:38:07 +0000

    feat(Main): adds logic to construct AddInstruction with reflection API

--- src/sml/Main.java
+++ src/sml/Main.java
@@ -1,6 +1,10 @@
 package sml;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.Map;
+
+import static sml.Registers.Register.*;
 
 public class Main {
 	/**
@@ -33,23 +37,104 @@ public class Main {
 
 			// Enum print works
 			System.out.println(Machine.getOpcodeClassName(supportedOpcodes.add));
+			try {
+				// Creates AddInstruction Class
+				Class<?> add = Class.forName(Machine.getOpcodeClassName(supportedOpcodes.add));
+				// Prints class name
+				System.out.println(add.getSimpleName());
+
+				// sets registers.
+				m.getRegisters().set(EAX, 5);
+				m.getRegisters().set(EBX, 6);
+
+				// int argumentLen = 2;
+				// String[] argumentsList = { "f3", "EAX", "EBX" };
+
+				Class<?> addIns = Class.forName(Machine.getOpcodeClassName(supportedOpcodes.add));
+				// AddInstruction(String label, RegisterName result, RegisterName source)
+				Constructor<?> constructor = addIns.getConstructor(new Class[]{ String.class, sml.RegisterName.class, sml.RegisterName.class });
+        Instruction ins = (Instruction)constructor.newInstance("f3", EAX, EBX);
+				System.out.println(ins.toString());
+
+				// for (Constructor<?> candidateConstructor : Class.forName(
+				// 		Machine.getOpcodeClassName(supportedOpcodes.add)).getConstructors()) {
+				// 	if (candidateConstructor.getParameterCount() == argumentLen) {
+				// 		try {
+				// 			Object[] parameterObjs = new Object[argumentLen];
+				// 			// get the candidate constructor parameters
+				// 			Class<?>[] paramCons = candidateConstructor.getParameterTypes();
+				// 			System.out.println("here 1");
+				// 			for (int i = 0; i < argumentLen; i++) {
+				// 				System.out.println("here 2");
+				// 				// attempt to type the parameters using any available string constructors
+				// 				// NoSuchMethodException will be thrown where retyping isn't possible
+				// 				Class<?> c = toWrapper(paramCons[i]);
+				// 				parameterObjs[i] = c.getConstructor(String.class).newInstance(argumentsList[i]);
+				// 			}
+				// 			// return instance ob object using the successful constructor
+				// 			// and parameters of the right class types.
+				// 			Object obj = candidateConstructor.newInstance(parameterObjs);
+				// 		} catch (NoSuchMethodException ignored) {
+				// 			System.out.println("NoSuchMethodException");
+				// 		} catch (Exception e) {
+				// 			e.printStackTrace();
+				// 		}
+				// 	}
+				// }
+
+				// Arguments
+				// int argumentLen = 3;
+				// Object[] parameterObjs = new Object[argumentLen];
+				// String[] argumentsList = { "null", "EAX", "EBX" };
+				// // get the candidate constructor parameters
+				// Class<?>[] paramCons = add.getConstructor().getParameterTypes();
+
+				// for (int i = 0; i < argumentLen; i++) {
+				// 	// attempt to type the parameters using any available string constructors
+				// 	// NoSuchMethodException will be thrown where retyping isn't possible
+				// 	Class<?> c = toWrapper(paramCons[i]);
+				// 	parameterObjs[i] = c.getConstructor(String.class).newInstance(argumentsList[i]);
+
+				// 	// return instance ob object using the successful constructor
+				// 	// and parameters of the right class types.
+				// 	Object obj = add.getConstructor().newInstance(parameterObjs);
+				// 	System.out.println(obj.toString());
+				// }
+				// Instantiate AddInstruction
+				// Object obj = add.getConstructor().newInstance(argumentsList);
+				// System.out.println(obj.toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
 			// Main.getEnumConstants();
 
 			// Class<> myClass = Main.class;
-			
+
 			// System.out.println(Arrays.toString(OpCodes.values()));
 			// System.out.println((OpCodes.values());
 
 			// for(OpCodes opcode : myClass.getEnumConstants()) {
-			// 	System.out.println(opcode.className());
+			// System.out.println(opcode.className());
 			// }
 
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error reading the program from " + args[0]);
 		}
 	}
+
+	private static Class<?> toWrapper(Class<?> testClass) {
+		return PRIMITIVE_TYPE_WRAPPERS.getOrDefault(testClass, testClass);
+	}
+
+	private static final Map<Class<?>, Class<?>> PRIMITIVE_TYPE_WRAPPERS = Map.of(
+			int.class, Integer.class,
+			long.class, Long.class,
+			boolean.class, Boolean.class,
+			byte.class, Byte.class,
+			char.class, Character.class,
+			float.class, Float.class,
+			double.class, Double.class,
+			short.class, Short.class,
+			void.class, Void.class);
 }
-
-
