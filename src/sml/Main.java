8ef76c5b959202bf525bfdaae1e909ec2904dc72
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-03-17 13:14:26 +0000

    feat(Main): adds test to print labels

--- src/sml/Main.java
+++ src/sml/Main.java
@@ -27,6 +27,9 @@ public class Main {
 			System.out.println("Ending program execution.");
 
 			System.out.println("Values of registers at program termination:" + m.getRegisters() + ".");
+
+			// My tests
+			System.out.println("Labels are:" + m.getLabels().toString() + ".");
 		}
 		catch (IOException e) {
 			System.out.println("Error reading the program from " + args[0]);
